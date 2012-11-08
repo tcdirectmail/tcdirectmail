22e8854953fa9921b9ed307fae875868ec76b78f
Jose Antonio Guerra <jaguerra@icti.es>
2012-11-08 13:31:17 +0100

    [Task] EV version 3.0.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Directmail extension with simple to setup and use mailer, and a very extensible recipient configuration.',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '2.0.2',
+	'version' => '3.0.0-dev',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
-			'typo3' => '4.0.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
