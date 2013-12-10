a57de5fc2198521b429f1719fad8f808132343b2
Jose Antonio Guerra <jaguerra@icti.es>
2013-12-10 18:24:46 +0100

    [TASK] Update dependency list

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.0.0-0.0.0',
-			'typo3' => '4.5.0-0.0.0',
+			'php' => '5.2.0-5.5.99',
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
