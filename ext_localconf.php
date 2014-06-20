13e4cf4f6ca33737b461496afbb8d1afe880a5d4
Daniel Schledermann <daniel@schledermann.net>
2014-06-20 21:13:06 +0200

    [TASK] Remove kickstarter integration.

--- ext_localconf.php
+++ ext_localconf.php
@@ -20,12 +20,6 @@ $TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['includeClassFiles'] = array(
 if (!isset($TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Mailer'])) $TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Mailer'] = 'TYPO3 CMS - tcdirectmail extension';
 if (!isset($TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Precedence'])) $TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Precedence'] = 'bulk';
 if (!isset($TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Provided-by'])) $TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Sponsored-by'] = 'http://www.casalogic.dk/ - Open Source Experts.';
-$TYPO3_CONF_VARS['EXTCONF']['kickstarter']['sections']['tx_tcdirectmail_targets'] = array(
-    'classname' => 'tx_tcdirectmail_section_targets',
-    'filepath' => 'EXT:tcdirectmail/sections/class.tx_tcdirectmail_section_targets.php',
-    'title' => 'Make a new directmail target',
-    'description' => 'Create additional directmail targets, based on your own tables.',
-);
 
 /** * Registering class to scheduler
 */
