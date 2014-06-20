39f2e845e7a92c10e213a3ea2c77921a1f629dce
Daniel Schledermann <daniel@schledermann.net>
2014-06-20 15:39:11 +0200

    [TASK] Update ext_emconf with proper contact info

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,15 +27,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => 'pages,tt_address,be_users,fe_users',
 	'clearcacheonload' => 1,
 	'lockType' => '',
-	'author' => 'Daniel Schledermann',
-	'author_email' => 'info@tcdirectmail.dk',
-	'author_company' => 'Casalogic A/S',
+	'author' => 'Daniel Schledermann, Jose Antonio Guerra',
+	'author_email' => 'typo3@ds.schledermann.net',
+	'author_company' => 'Linkfactory A/S',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-5.5.99',
-			'typo3' => '4.5.0-6.1.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
