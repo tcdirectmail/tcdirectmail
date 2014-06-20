440b971bd47a0e2718ae9bc8dbfe94cd8d552e96
Daniel Schledermann <daniel@schledermann.net>
2014-06-20 21:20:42 +0200

    [TASK] Clean out old pre-autoload class includes.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,4 @@
 <?php
-
-$tempFilePath = t3lib_extMgm::extPath('tcdirectmail');
-$TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['includeClassFiles'] = array(
-    $tempFilePath.'class.tx_tcdirectmail_target_beusers.php',
-    $tempFilePath.'class.tx_tcdirectmail_target_csvfile.php',
-    $tempFilePath.'class.tx_tcdirectmail_target_fegroups.php',
-    $tempFilePath.'class.tx_tcdirectmail_target_fepages.php',
-    $tempFilePath.'class.tx_tcdirectmail_target_html.php',
-    $tempFilePath.'class.tx_tcdirectmail_target_csvlist.php',
-    $tempFilePath.'class.tx_tcdirectmail_target_csvurl.php',
-    $tempFilePath.'class.tx_tcdirectmail_target_rawsql.php',
-    $tempFilePath.'class.tx_tcdirectmail_target_ttaddress.php',
-    $tempFilePath.'class.tx_tcdirectmail_plain_html2text.php',
-    $tempFilePath.'class.tx_tcdirectmail_plain_lynx.php',
-    $tempFilePath.'class.tx_tcdirectmail_plain_simple.php',
-    $tempFilePath.'class.tx_tcdirectmail_plain_template.php');
-    
    
 if (!isset($TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Mailer'])) $TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Mailer'] = 'TYPO3 CMS - tcdirectmail extension';
 if (!isset($TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Precedence'])) $TYPO3_CONF_VARS['EXTCONF']['tcdirectmail']['extraMailHeaders']['X-Precedence'] = 'bulk';
