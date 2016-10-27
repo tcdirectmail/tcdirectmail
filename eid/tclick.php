00c90a9932d50f706d91bd1eb66f5fb07b518f91
Daniel Schledermann <daniel@schledermann.net>
2016-10-27 15:35:11 +0200

    [BUGFIX|SECURITY] Fix potential XSS in test link clicks

--- eid/tclick.php
+++ eid/tclick.php
@@ -3,5 +3,15 @@
  * This is the test version of the click.php script
  */
 
-header ('Location: '.base64_decode($_REQUEST['l']));
+
+$l = base64_decode($_REQUEST['l']);
+$c = $_REQUEST['c'];
+
+if (t3lib_div::stdAuthCode($l) == $c) {
+	header ('Location: ' . $l);
+}
+else {
+	header('HTTP/1.1 503 Service unavailable');
+	echo "The test link was broken.";
+}
 
